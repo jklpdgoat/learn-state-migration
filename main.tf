84f5025ef9d3ea2ae3696e63f0ddeff30282c2d4
jklpdgoat <jarpadul@gmail.com>
2023-03-15 01:19:33 +0800

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "9"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "_"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
